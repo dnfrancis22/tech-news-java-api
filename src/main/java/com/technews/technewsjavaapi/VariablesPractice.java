09c0375fce9dec0cdf2c977f416e49965aff4e9a
Dennis Francis <dnfrancis22@gmail.com>
2021-05-17 14:17:20 -0700

    "created Variable Practice program"

---
+++ src/main/java/com/technews/technewsjavaapi/VariablesPractice.java
@@ -0,0 +1,20 @@
+package com.technews.technewsjavaapi;
+
+public class VariablesPractice {
+
+    public static void main(String[] args) {
+
+        int a = 7;
+        double b = 5.5;
+        String c = "dog";
+
+        double sum = a + b;
+
+        String concat = a + c;
+
+        System.out.println(sum);
+
+        System.out.println(concat);
+
+    }
+}
---
+++ src/test/java/com/technews/technewsjavaapi/string.java
@@ -0,0 +1,4 @@
+package com.technews.technewsjavaapi;
+
+public class string {
+}
