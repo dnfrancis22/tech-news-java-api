706ca90330747bbb7984e54c0ba1f43a590b460c
Dennis Francis <dnfrancis22@gmail.com>
2021-05-18 09:57:36 -0700

    "Create the Instance Variable-Level Annotations for the User Class"

--- src/main/java/com/technews/model/User.java
+++ src/main/java/com/technews/model/User.java
@@ -1,12 +1,27 @@
 package com.technews.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "user")
-public class User {
+public class User<Vote, Post, Comment> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String username;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    @Transient
+    boolean loggedIn;
+
+    private List<Post> posts;
+    private List<Vote> votes;
+    private List<Comment> comments;
 
 }
